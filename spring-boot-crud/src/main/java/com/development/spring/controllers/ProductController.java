c0daee2f0ceda9be288e7ce5f5d82d04b14ca174
Sanjib Bhadra <sanjivss1989@gmail.com>
2020-03-09 13:53:37 +0530

    Swagger 2

--- spring-boot-crud/src/main/java/com/development/spring/controllers/ProductController.java
+++ spring-boot-crud/src/main/java/com/development/spring/controllers/ProductController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -30,6 +31,7 @@ import com.development.spring.service.ProductService;
  *
  */
 @RestController
+@RequestMapping(value = "/product")
 public class ProductController {
 	
 	@Autowired
